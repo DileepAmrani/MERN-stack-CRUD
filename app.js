3d21be5db4ad207979602fdd8c880aec539fc574
DileepAmrani <amranidileep44@gmail.com>
2020-07-12 08:17:36 -0700

    Mern stack two nodes with connection

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+mongoose.connect('mongodb+srv://dileep44:amrani786@cluster0.da90r.mongodb.net/practice?retryWrites=true&w=majority').then(()=>{
+  app.listen(5000);
+})
+.catch((err)=>{
+  console.log(err)
+})
---
+++ util/location.js
@@ -0,0 +1,8 @@
+function getCoordsForAddress(address) {
+  return {
+    lat: 40.7484474,
+    lng: -73.9871516
+  }
+  
+}
+module.exports = getCoordsForAddress;
